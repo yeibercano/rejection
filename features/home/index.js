ae80c6f827d557516df0e8f504d624f19ff54f23
Yeiber <yeibercano@gmail.com>
2017-03-26 15:59:15 -0700

    create score component

--- features/home/index.js
+++ features/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../footer'
 import Header from '../header';
+import Main from '../main';
 
 export default () => (
   <div>
     <Header />
+    <Main />
     <h1 className="title">Rejection</h1>
     <Footer />
   </div>
---
+++ features/score/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <aside>
+    <article>
+      <h1>SCORE</h1>
+      <span className="score">10</span>
+    </article>
+  </aside>
+);
