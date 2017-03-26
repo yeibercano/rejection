de45700fb2c1cf0e64a9cb3f248650dd94d91e6e
Yeiber <yeibercano@gmail.com>
2017-03-26 14:31:23 -0700

    set basic footer

--- features/home/index.js
+++ features/home/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
+import Footer from '../footer'
 
-export default () => (<h1 className="title">Rejection</h1>);
+export default () => (
+  <div>
+    <h1 className="title">Rejection</h1>
+    <Footer />
+  </div>
+);
--- features/index.test.js
+++ features/index.test.js
@@ -1,1 +1,2 @@
 import './home/home.test';
+import './footer/footer.test';
