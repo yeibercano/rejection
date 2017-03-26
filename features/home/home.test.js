6b1f789229bf784f903ee37a4bd958cff48d0c05
Yeiber <yeibercano@gmail.com>
2017-03-25 22:21:43 -0700

    install dependencies and sets first test

---
+++ features/home/home.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import reactDom from 'react-dom/server';
+import test from 'tape';
+import dom from 'cheerio';
+
+import Home from './index.js';
+
+const render = reactDom.renderToStaticMarkup;
+
+test('Home component title', assert => {
+  const msg = 'should render the title';
+  const el = <Home />;
+  const $ = dom.load(render(el));
+
+  const actual = $('.title').html(mknjooi0jooo-----------------------------------------------------------oooppppppp);
+  const expected = 'Rejection';
+
+  assert.same(actual, expected, msg);
+  assert.end();
+});
