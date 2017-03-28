d80b0fe29065097fde5e4f9770f6fa819d50f6cf
Yeiber <yeibercano@gmail.com>
2017-03-27 23:05:16 -0700

    clean code style

--- features/reducers.js
+++ features/reducers.js
@@ -3,11 +3,10 @@ import { accept } from './entries/entry_accept_reducer';
 import { reject } from './entries/entry_reject_reducer';
 import { score } from './entries/entry_score_reducer';
 
-
- const reducers = combineReducers({
- 	accept,
+const reducers = combineReducers({
+  accept,
   reject,
   score
- })
+})
 
 export default reducers;
