c1a967827682ff233c20b9f026ff693295d661e5
Yeiber <yeibercano@gmail.com>
2017-03-31 15:29:45 -0700

    remove extra function/console

---
+++ utilities/index.js
@@ -0,0 +1,19 @@
+export const localStorageDefaults = {
+  score: 0,
+  accept: [],
+  reject: []
+}
+
+export const checkSetKeys = function (defaults) {
+  for(var key in defaults) {
+    if (Array.isArray(defaults[key])) {
+      if (localStorage.getItem(key) === null ) {
+        localStorage.setItem(key, [] )
+      }
+    } else {
+      if (localStorage.getItem(key) === null) {
+        localStorage.setItem('score', 0 )
+      }
+    }
+  }
+}
