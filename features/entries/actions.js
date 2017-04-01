54649443474403555d434353e2606273a104abc4
Yeiber <yeibercano@gmail.com>
2017-03-31 21:42:10 -0700

    expand actions to handle entry input

--- features/entries/actions.js
+++ features/entries/actions.js
@@ -1,23 +1,33 @@
 export const accept = (entry) => {
   return dispatch => {
     const getScore = Number(localStorage.getItem('score'));
-    const currentScore = getScore + 1;
+    const updatedScore = getScore + 1;
+    // grab current entries -> []
+    const entries = localStorage.getItem('accept');
     const currentEntry = {...entry, timestamp: new Date().getTime()}
-
-    dispatch({ type: 'ACCEPT', payload: currentEntry })
-    dispatch({ type: 'UPDATE_SCORE', payload: currentScore })
-    localStorage.setItem('score', currentScore);
+    const updatedEntries = [...entries, currentEntry];
+    // sending on the dispatch to reducers
+    dispatch({ type: 'ACCEPT', payload: updatedEntries })
+    dispatch({ type: 'UPDATE_SCORE', payload: updatedScore })
+    // set it to localStorage /score and entries
+    localStorage.setItem('score', updatedScore);
+    localStorage.setItem('accept', updatedEntries);
   }
 }
 
 export const reject = (entry) => {
   return dispatch => {
     const getScore = Number(localStorage.getItem('score'));
-    const currentScore = getScore + 10;
-    const currentEntry = {...entry, timestamp: new Date().getTime()}
+    const updatedScore = getScore + 10;
 
-    dispatch({ type: 'REJECT', payload: currentEntry })
-    dispatch({ type: 'UPDATE_SCORE', payload: currentScore })
-    localStorage.setItem('score', currentScore);
+    const currentEntry = {...entry, timestamp: new Date().getTime()}
+    const entries = localStorage.getItem('reject');
+    const updatedEntries = [...entries, currentEntry];
+
+    dispatch({ type: 'REJECT', payload: updatedEntries })
+    dispatch({ type: 'UPDATE_SCORE', payload: updatedScore })
+
+    localStorage.setItem('score', updatedScore);
+    localStorage.setItem('reject', updatedEntries);
   }
 }
