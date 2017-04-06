20eea428e6dc2b2723e19a88b4fd80447358176a
Yeiber <yeibercano@gmail.com>
2017-04-06 13:46:55 -0700

    refactor to entry to presentational component

--- features/entries/index.js
+++ features/entries/index.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { accept, reject } from './actions';
 import { Field, reduxForm, reset } from 'redux-form';
+import Entry from './entry/index';
 
-class Entries extends Component {
+export class Entries extends Component {
   handleEntrySubmit(entry) {
     if (entry.status === "accept") {
       this.props.accept(entry);
@@ -24,25 +25,7 @@ class Entries extends Component {
             <b>Win</b> = 1 point. <b>Rejection</b> = 10 points.<br/>
             How long can you make your rejection streak last?
           </p>
-          <form className="entryForm" onSubmit={ handleSubmit((e) => this.handleEntrySubmit(e)) }>
-            <h3 className="subTitle">Entry</h3>
-            <div>
-              <label htmlFor="askee">Name</label>
-              <Field name="askee" component="input" type="text" placeholder="name" className="askee" required/>
-            </div>
-            <div>
-              <label htmlFor="ask">Question</label>
-              <Field name="ask" component="input" type="text" placeholder="question" className="ask" required/>
-            </div>
-            <div>
-              <label>Reponse</label>
-              <div>
-                <label><Field name="status" component="input" type="radio" value="accept" required/> accept</label>
-                <label><Field name="status" component="input" type="radio" value="reject" required/> reject</label>
-              </div>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
+          <Entry handleSubmit={handleSubmit((e) => this.handleEntrySubmit(e)) }/>
       </section>
     );
   }
