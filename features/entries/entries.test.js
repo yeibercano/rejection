c94ffde230777d75b1b818f3d9589b4c7813c887
Yeiber <yeibercano@gmail.com>
2017-04-07 15:46:18 -0700

    create nested tests

--- features/entries/entries.test.js
+++ features/entries/entries.test.js
@@ -9,14 +9,46 @@ import { store } from '../../utilities'
 
 const render = reactDom.renderToStaticMarkup;
 
-test('Entries component', assert => {
-  const el = <Provider store={store}><Entries /></Provider>;
-  const $ = dom.load(render(el));
+test('Entries component', nest => {
 
-  const msg = 'should render 4 input';
-  const actual = $('input').length;
-  const expected = 4;
+  nest.test('inputs', assert => {
+    const el = <Provider store={store}><Entries /></Provider>;
+    const $ = dom.load(render(el));
 
-  assert.same(actual, expected, msg);
-  assert.end();
+    const msg = 'should render 4 input';
+    const actual = $('input').length;
+    const expected = 4;
+
+    assert.same(actual, expected, msg);
+    assert.end();
+  });
+
+  nest.test('title', assert => {
+    const el = <Provider store={store}><Entries /></Provider>;
+    const $ = dom.load(render(el));
+    const title = 'Rejection';
+
+    const msg = 'should render a Rejection title';
+    const actual = $('.title').html();
+    const expected = title;
+
+    assert.same(actual, expected, msg);
+    assert.end();
+  });
+
+  nest.test('paragraph', assert => {
+    const el = <Provider store={store}><Entries /></Provider>;
+    const $ = dom.load(render(el));
+    const title = 'Rejection';
+
+    const msg = 'should render a paragraph with content';
+    const output = $('p.intro').children().length;
+
+    const actual = output > 0;
+    const expected = true;
+
+    assert.same(actual, expected, msg);
+    assert.end();
+  });
+
 });
