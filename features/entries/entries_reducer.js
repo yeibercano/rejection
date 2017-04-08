b5ec7503811a6e2d8008914c0041e85e5e416ae4
Yeiber <yeibercano@gmail.com>
2017-04-08 00:12:31 -0700

    condese reducers with actions and constants

---
+++ features/entries/entries_reducer.js
@@ -0,0 +1,71 @@
+const ACCEPT = 'ACCEPT',
+      REJECT = 'REJECT',
+      CURRENT_SCORE = 'CURRENT_SCORE';
+
+// action creators
+export const accept = (entry) => {
+  return dispatch => {
+    const getScore = JSON.parse(localStorage.getItem('score'));
+    const updatedScore = getScore + 1;
+    // grab current entries -> []
+    const entries = JSON.parse(localStorage.getItem('accept'));
+    const currentEntry = {...entry, timestamp: Date.now()}
+    const updatedEntries = [...entries, currentEntry];
+    // sending on the dispatch to reducer
+    dispatch({ type: ACCEPT, payload: currentEntry, updatedScore })
+    // set it to localStorage /score and entries
+    localStorage.setItem('score', JSON.stringify(updatedScore));
+    localStorage.setItem('accept', JSON.stringify(updatedEntries));
+  }
+}
+export const reject = (entry) => {
+  return dispatch => {
+    const getScore = JSON.parse(localStorage.getItem('score'));
+    const updatedScore = getScore + 10;
+
+    const currentEntry = {...entry, timestamp: Date.now()}
+    const entries = JSON.parse(localStorage.getItem('reject'));
+    const updatedEntries = [...entries, currentEntry];
+
+    dispatch({ type: REJECT, payload: currentEntry, updatedScore })
+
+    localStorage.setItem('score', JSON.stringify(updatedScore));
+    localStorage.setItem('reject', JSON.stringify(updatedEntries));
+  }
+}
+
+// reducers
+const defaultState = {
+  entries:[],
+  currentScore: null
+}
+
+export function entries (state = defaultState, action = {}) {
+	const { type, payload, currentScore } = action;
+	switch (type) {
+    case CURRENT_SCORE:
+      return {
+        ...state,
+        currentScore: payload || state.currentScore || 0
+        }
+      case ACCEPT:
+        return {
+          ...state,
+          entries: [
+            ...state.entries,
+            payload
+          ],
+          currentScore: state.currentScore + 1
+        }
+      case REJECT:
+        return {
+          ...state,
+          entries: [
+            ...state.entries,
+            payload
+          ],
+          currentScore: state.currentScore + 10
+        }
+      default: return state
+	}
+}
