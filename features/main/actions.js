79a132d76ded18868264e5243c0349bd7bbad81b
Yeiber <yeibercano@gmail.com>
2017-04-07 23:44:05 -0700

    clean payload

--- features/main/actions.js
+++ features/main/actions.js
@@ -3,7 +3,6 @@ const CURRENT_SCORE = 'CURRENT_SCORE';
 export const currentScore = () => {
   return dispatch => {
     const getItem = Number(localStorage.getItem('score'));
-    console.log('getItem', getItem)
-      dispatch({ type: CURRENT_SCORE, payload: getItem || 0 });
+      dispatch({ type: CURRENT_SCORE, payload: getItem });
   }
 }
