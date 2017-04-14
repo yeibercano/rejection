099badfa180b10bf267f5ae797348114dc9a7adb
Yeiber <yeibercano@gmail.com>
2017-04-14 08:46:42 -0700

    refactor to separate action creators

--- features/main/actions.js
+++ features/main/actions.js
@@ -1,8 +1,9 @@
 const CURRENT_SCORE = 'CURRENT_SCORE';
+const getCurrentScore = payload => ({ type: CURRENT_SCORE, payload });
 
 export const currentScore = () => {
   return dispatch => {
     const getItem = Number(localStorage.getItem('score'));
-      dispatch({ type: CURRENT_SCORE, payload: getItem });
+      dispatch(getCurrentScore(getItem));
   }
 }
