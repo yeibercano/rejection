8ae422aff8c78d707858ecbb9b1e9e885ac75955
Yeiber <yeibercano@gmail.com>
2017-04-15 17:01:21 -0700

    export test sagas, working

--- features/sagas.js
+++ features/sagas.js
@@ -1,3 +1,8 @@
+//watchAddQuestion sagas
+export const watchAddQuestion = function* watchAddQuestion() {
+  console.log('redux-sagas runnig');
+}
+
 //entry poin to all sagas // Ccombine sagas type
 export default function* rootSaga() {
   yield [
