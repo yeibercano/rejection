5263d6c928b0e7d8022b92addbc56551483f6338
Yeiber <yeibercano@gmail.com>
2017-04-17 15:32:37 -0700

    set localStorage logic to add questions - load state

--- features/sagas.js
+++ features/sagas.js
@@ -1,15 +1,18 @@
-import { takeEvery } from 'redux-saga';
-import { put, call } from 'redux-saga/effects';
+import { put, call, takeEvery } from 'redux-saga/effects';
 
 //actions
-const ADD_QUESTION = 'ADD_QUESTION';
+const ADD_QUESTION = 'ADD_QUESTION', NEW_STATE = 'NEW_STATE', LOAD_STATE = 'LOAD_STATE';
+const addQuestion = question => ({ type: ADD_QUESTION, payload: question });
+const updateQuestions = questions => ({ type: NEW_STATE, payload: questions });
+
 
 //worker saga
-export function* addQuestionAsync(payload) {
+export function* addQuestionAsync(action) {
   try {
     const parsedAsks = JSON.parse(localStorage.getItem('asks'));
-    const newAsks = [...parsedAsks, payload];
+    const newAsks = [...parsedAsks, action.payload];
     localStorage.setItem('asks', JSON.stringify(newAsks));
+    yield put(updateQuestions(newAsks));
   } catch (e) {
     console.log(e);
   }
@@ -19,9 +22,24 @@ export function* watchAddQuestion() {
   yield takeEvery(ADD_QUESTION, addQuestionAsync);
 }
 
+//worker saga
+export function* updateQuestionsAsync() {
+  try {
+    const parsedAsks =  yield JSON.parse(localStorage.getItem('asks'));
+    yield put(updateQuestions(parsedAsks));
+  } catch (e) {
+    console.log(e);
+  }
+}
+//watcher saga - AddQuestion
+export function* watchUpdateQuestions() {
+  yield takeEvery(LOAD_STATE, updateQuestionsAsync);
+}
+
 //entry poin to all sagas // Ccombine sagas type
 export default function* rootSaga() {
   yield [
     watchAddQuestion(),
+    watchUpdateQuestions(),
   ]
 }
