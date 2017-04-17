454db031dfdd2eaa9d7506f5ad2c64d4a12c1b0d
Yeiber <yeibercano@gmail.com>
2017-04-16 23:15:42 -0700

    set up ADD_QUESTION saga

--- features/sagas.js
+++ features/sagas.js
@@ -1,6 +1,22 @@
-//watchAddQuestion sagas
-export const watchAddQuestion = function* watchAddQuestion() {
-  console.log('redux-sagas runnig');
+import { takeEvery } from 'redux-saga';
+import { put, call } from 'redux-saga/effects';
+
+//actions
+const ADD_QUESTION = 'ADD_QUESTION';
+
+//worker saga
+export function* addQuestionAsync(payload) {
+  try {
+    const parsedAsks = JSON.parse(localStorage.getItem('asks'));
+    const newAsks = [...parsedAsks, payload];
+    localStorage.setItem('asks', JSON.stringify(newAsks));
+  } catch (e) {
+    console.log(e);
+  }
+}
+//watcher saga - AddQuestion
+export function* watchAddQuestion() {
+  yield takeEvery(ADD_QUESTION, addQuestionAsync);
 }
 
 //entry poin to all sagas // Ccombine sagas type
