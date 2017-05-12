cef08eb47872078b45fec042a66d4f625709af4d
Yeiber <yeibercano@gmail.com>
2017-05-12 00:11:38 -0700

    remove actions and import them fromr reducer

--- features/entries/entries_sagas.js
+++ features/entries/entries_sagas.js
@@ -1,15 +1,9 @@
 import { put, call, takeEvery, select } from 'redux-saga/effects';
-import { selectQuestions } from './entries_reducer';
+import { selectQuestions, addQuestion, fetchedQuestions, addedQuestion } from './entries_reducer';
 
 //declarative effects
 export const getQuestionFromLocalStorage = () => JSON.parse(localStorage.getItem('asks'));
 export const setAksInLocalStorage = asks => { localStorage.setItem('asks', JSON.stringify(asks)) }
-
-//actions - action creators
-const ADD_QUESTION = 'ADD_QUESTION', FETCHED_QUESTIONS = 'FETCHED_QUESTIONS', LOAD_STATE = 'LOAD_STATE', ADDED_QUESTION  = 'ADDED_QUESTION';
-const addQuestion = question => ({ type: ADD_QUESTION, payload: question });
-const fetchedQuestions = questions => ({ type: FETCHED_QUESTIONS, payload: questions });
-const addedQuestion = () => ({ type: ADDED_QUESTION });
 
 //worker/task saga addQuestionAsync
 export function* addQuestionToStorage() {
@@ -24,7 +18,7 @@ export function* addQuestionToStorage() {
 
 //watcher saga - addQuestionAsync
 function* watchAddQuestion() {
-  yield takeEvery(ADD_QUESTION, addQuestionToStorage);
+  yield takeEvery(addQuestion().type, addQuestionToStorage);
 }
 //worker saga fetchedQuestionsAsync
 export function* fetchQuestionsAsync() {
@@ -37,7 +31,7 @@ export function* fetchQuestionsAsync() {
 }
 //watcher saga - fetchedQuestionsAsync
 function* watchUpdateQuestions() {
-  yield takeEvery(LOAD_STATE, fetchQuestionsAsync);
+  yield takeEvery(loadState().type, fetchQuestionsAsync);
 }
 
 // entry point to all sagas combine sagas type
