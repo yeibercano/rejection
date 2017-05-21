d292fd09278742174d7868860bd84e23121c0272
Yeiber <yeibercano@gmail.com>
2017-05-20 18:13:43 -0700

    initialize app and database

---
+++ app/storage/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+
+const config = {
+   apiKey: "AIzaSyDApnmSK2Fpu6BDWBZuCfCyZXmuKzOG2dE",
+   authDomain: "rejection-80735.firebaseapp.com",
+   databaseURL: "https://rejection-80735.firebaseio.com",
+   projectId: "rejection-80735",
+   storageBucket: "rejection-80735.appspot.com",
+   messagingSenderId: "282714843125"
+ };
+
+firebase.initializeApp(config)
+
+export default firebase;
+
+export const database = firebase.database();
