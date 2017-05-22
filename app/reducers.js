7375ce1dafc184cf9cbe9ad396281bd4fa10d4ca
Yeiber <yeibercano@gmail.com>
2017-05-21 23:30:24 -0700

    rename reducer

--- app/reducers.js
+++ app/reducers.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-import asks from './features/entries/entries_reducer';
+import asksReducer from './features/entries/entries_reducer';
 import { reducer as form } from 'redux-form';
 
 const reducers = combineReducers({
-  asks,
+  asksReducer,
   form
 })
 
