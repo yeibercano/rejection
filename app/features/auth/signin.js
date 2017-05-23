984360b67f189fa2547f294f0dab2b4b63741143
Yeiber <yeibercano@gmail.com>
2017-05-22 19:48:13 -0700

    add indentantion

--- app/features/auth/signin.js
+++ app/features/auth/signin.js
@@ -3,15 +3,15 @@ import { auth, googleAuthProvider } from '../../storage/firebase';
 const Signin = () => {
   return (
     <aside className="signinComponent">
-        <button className="signin" onClick={() => auth.signInWithPopup(googleAuthProvider) }>
-          Sign In
-        </button>
-        <style jsx>{`
-          .signinComponent {
-            width: auto;
-          }
-        `}
-        </style>
+      <button className="signin" onClick={() => auth.signInWithPopup(googleAuthProvider) }>
+        Sign In
+      </button>
+      <style jsx>{`
+        .signinComponent {
+          width: auto;
+        }
+      `}
+      </style>
     </aside>
   );
 }
