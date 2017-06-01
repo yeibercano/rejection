c8e6415a2dee69395e4aaf7b93a26f81de4a884d
Yeiber <yeibercano@gmail.com>
2017-05-31 22:31:39 -0700

    watch signInUserAsync saga test fail

--- app/index.test.js
+++ app/index.test.js
@@ -8,3 +8,4 @@ import './features/entries/entries.reducer.test';
 import './features/entries/entries.sagas.test';
 import './features/auth/signin.test';
 import './features/auth/auth.reducer.test';
+import './features/auth/auth.signin.saga.test';
