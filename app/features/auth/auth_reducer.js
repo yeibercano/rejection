cb4f552be54a13b48a093b1595214c0a8935abd8
Yeiber <yeibercano@gmail.com>
2017-05-31 21:01:19 -0700

    add userLoggedIn action creator

--- app/features/auth/auth_reducer.js
+++ app/features/auth/auth_reducer.js
@@ -3,7 +3,17 @@ const defaultState = {
   user: {}
 }
 
+const USER_LOGGED_IN = 'USER_LOGGED_IN';
+export const userLoggedIn = (user={}) => ({type:USER_LOGGED_IN, payload: user});
+
 export default (state=defaultState, action={}) => {
   const { type, payload } = action;
-  return state;
+  switch(type) {
+    case USER_LOGGED_IN:
+    return {
+      ...state,
+      user: payload
+    }
+    default: return state;
+  }
 };
