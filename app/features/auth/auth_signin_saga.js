0e04969db95d48abf416727d11aa813d1ad60c27
Yeiber <yeibercano@gmail.com>
2017-06-02 00:08:50 -0700

    make setToken test pass

--- app/features/auth/auth_signin_saga.js
+++ app/features/auth/auth_signin_saga.js
@@ -2,11 +2,27 @@ import { put, call, takeEvery, take } from 'redux-saga/effects';
 import { userLoggedIn, userLogginRequested } from './auth_reducer';
 import { authUser, auth, googleAuthProvider } from '../../storage/firebase';
 
+//effects
+export const setToken = ({
+  uid = '',
+  displayName = '',
+  email = ''
+} = {}) => {
+  localStorage.setItem('uid', uid);
+  return {
+    uid,
+    displayName,
+    email
+  }
+};
+
 //worker saga - signInUserAsync
 export function* signInUserAsync() {
   try {
-    const loggedUser = yield call(authUser, auth, googleAuthProvider);
-    yield put(userLoggedIn(loggedUser.user.user));
+    const userAuth = yield call(authUser, auth, googleAuthProvider);
+    const user = userAuth ? userAuth.user.user : {};
+    yield call(setToken, user);
+    yield put(userLoggedIn(user));
 
   } catch (e) {
     console.log('error -->', e);
