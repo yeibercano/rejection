5849c0ca9bb4f7f8c4df249abf15f0662607a97a
Yeiber <yeibercano@gmail.com>
2017-06-22 13:51:20 -0700

    watch currentUser component test fail

---
+++ app/features/auth/currentUser.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import reactDom from 'react-dom/server';
+import test from 'tape';
+import dom from 'cheerio';
+
+import CurrentUser from './currentUser.js';
+
+const render = reactDom.renderToStaticMarkup;
+
+test('CurrentUser component', assert => {
+  const msg = 'should render a ul element';
+  const el = <CurrentUser />
+  const $ = dom.load(render(el));
+
+  const actual = $('ul').length;
+  const expected = 1;
+
+  assert.same(actual, expected, msg);
+  assert.end();
+
+});
