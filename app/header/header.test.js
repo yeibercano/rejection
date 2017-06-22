bd2c462cd460c92a724b38e8382b0518b220caf6
Yeiber <yeibercano@gmail.com>
2017-06-22 15:57:42 -0700

    connect auth components to store

--- app/header/header.test.js
+++ app/header/header.test.js
@@ -3,13 +3,16 @@ import reactDom from 'react-dom/server';
 import test from 'tape';
 import dom from 'cheerio';
 
+import { Provider } from 'react-redux';
+import { store } from '../utilities'
+
 import Header from './index.js';
 
 const render = reactDom.renderToStaticMarkup;
 
 test('Header component', assert => {
   const msg = 'should render Brand name';
-  const el = <Header />;
+  const el = <Provider store={store}><Header /></Provider>;
   const $ = dom.load(render(el));
 
   const actual = $('a').html();
