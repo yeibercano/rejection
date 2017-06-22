056e882a75cdf664344e8ff7ae67fd1d5a0e313f
Yeiber <yeibercano@gmail.com>
2017-06-22 14:04:31 -0700

    render ul element to pass currentUser comp test

---
+++ app/features/auth/currentUser.js
@@ -0,0 +1,17 @@
+
+const CurrentUser = ({ displayName, email }) => {
+  return (
+    <aside className="currentUserComponent">
+      <ul>
+      </ul>
+      <style jsx>{`
+        .currentUserComponent {
+          width: auto;
+        }
+      `}
+      </style>
+    </aside>
+  );
+}
+
+export default CurrentUser
