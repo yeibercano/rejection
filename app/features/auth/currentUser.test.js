0718d66c8cc2dee06e5fec8b0e9a3ed0c4c092c8
Yeiber <yeibercano@gmail.com>
2017-06-23 16:04:43 -0700

    add test for ul and .currentUser elements

--- app/features/auth/currentUser.test.js
+++ app/features/auth/currentUser.test.js
@@ -7,12 +7,26 @@ import CurrentUser from './currentUser.js';
 
 const render = reactDom.renderToStaticMarkup;
 
-test('CurrentUser component', assert => {
-  const msg = 'should render a ul element';
+test('CurrentUser component - ul', assert => {
+  const msg = 'should render ul element';
   const el = <CurrentUser />
+
   const $ = dom.load(render(el));
 
   const actual = $('ul').length;
+  const expected = 1;
+
+  assert.same(actual, expected, msg);
+  assert.end();
+
+});
+
+test('CurrentUser component - .currentUser', assert => {
+  const msg = 'should render aside .currentUser element';
+  const el = <CurrentUser />
+  const $ = dom.load(render(el));
+
+  const actual = $('aside.currentUser').length;
   const expected = 1;
 
   assert.same(actual, expected, msg);
