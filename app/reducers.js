c6616d0e08a45c6819d0e9328c771c047db890c5
Yeiber <yeibercano@gmail.com>
2017-08-04 13:13:08 -0700

    refactor asksReducer

--- app/reducers.js
+++ app/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import asksReducer from './features/entries/entries_reducer';
+import { asksReducer } from './features/entries/entries_reducer';
 import userReducer from './features/auth/auth_reducer';
 
 import { reducer as form } from 'redux-form';
@@ -8,6 +8,6 @@ const reducers = combineReducers({
   asksReducer,
   userReducer,
   form
-})
+});
 
 export default reducers;
