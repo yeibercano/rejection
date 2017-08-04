82ac9847ea02a74d33c8e4937f41e8c15c30e8ca
Yeiber <yeibercano@gmail.com>
2017-08-03 21:15:19 -0700

    refactor reducer

--- app/features/entries/entries_reducer.js
+++ app/features/entries/entries_reducer.js
@@ -1,62 +1,54 @@
 import cuid from 'cuid';
+import dux from 'autodux';
 
-// actions
 const ADD_QUESTION = 'ADD_QUESTION';
-const FETCHED_QUESTIONS = 'FETCHED_QUESTIONS';
-const LOAD_STATE = 'LOAD_STATE';
+
+const Accepted = 'Accepted';
+const Rejected = 'Rejected';
+const Unanswered = 'Unanswered';
+const NOT_ANSWERED_TIME = 0;
+const DEFAULT_ID = 0;
 
 // action creators
-export const addQuestion = ({
-  status = '',
-  ask = '',
-  askee = '',
-  timeStamp = Date.now(),
-  id = cuid()
-} = {}) => ({
+const addQuestion = (
+  {
+    status = '',
+    ask = '',
+    askee = '',
+    askedTime = Date.now(),
+    answeredTime = NOT_ANSWERED_TIME,
+    id = cuid()
+  } = {}
+) => ({
   type: ADD_QUESTION,
   payload: {
     status,
     ask,
     askee,
-    timeStamp,
+    askedTime,
+    answeredTime,
     id
   }
 });
 
-export const fetchedQuestions = (ask = {}) => ({
-  type: FETCHED_QUESTIONS,
-  payload: ask
-});
-
-export const loadState = () => ({ type: LOAD_STATE });
-
 const defaultState = [];
 
-// reducer
-export default (state = defaultState, action = {}) => {
+const reducer = (state = [], action = {}) => {
   const { type, payload } = action;
   switch (type) {
-    case FETCHED_QUESTIONS:
-      return state.concat(payload);
     case ADD_QUESTION:
       return state.concat([payload]);
-
-    default: return state;
+    default:
+      return state;
   }
 };
 
 // selectors
 export const getSlice = state => state.asksReducer;
-export const selectQuestions = state => getSlice(state).asks;
 
-export const currentScoreReducer = (acc, cur) => {
-  if (cur.status === 'ACCEPT') {
-    return acc + 1;
-  } else if (cur.status === 'REJECT') {
-    return acc + 10;
-  }
-
-  return acc + 0;
-}
-
-export const getScore = state => getSlice(state).asks.reduce(currentScoreReducer, 0);
+const getScore = state =>
+  getSlice(state).reduce(
+    (accu, { status }) =>
+      status === Accepted ? accu + 1 : status === Rejected ? accu + 10 : accu,
+    0
+  );
