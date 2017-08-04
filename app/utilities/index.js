dca81e3041ba89f6fdf507c228e9a8f102e7b83c
Yeiber <yeibercano@gmail.com>
2017-08-04 14:11:26 -0700

    refactor utilities

--- app/utilities/index.js
+++ app/utilities/index.js
@@ -2,16 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import reducers from '../reducers';
-import rootSaga from '../sagas'
+import rootSaga from '../sagas';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
 const logger = createLogger();
-export const store = createStore(reducers, composeWithDevTools(applyMiddleware(sagaMiddleware,logger)));
+export const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(rootSaga);
 
 const LOAD_STATE = 'LOAD_STATE';
 const USER_LOGGIN_REQUESTED = 'USER_LOGGIN_REQUESTED';
 
 export const loadState = () => store.dispatch({ type: LOAD_STATE });
-export const userLogginRequested = () => store.dispatch({ type: USER_LOGGIN_REQUESTED });
+export const userLogginRequested = () =>
+  store.dispatch({ type: USER_LOGGIN_REQUESTED });
