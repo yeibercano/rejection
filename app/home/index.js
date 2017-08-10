79c2d2f9982b9867d4d059635e0bb54b0ca6c62f
Yeiber <yeibercano@gmail.com>
2017-08-10 14:22:01 -0700

    fix styling

--- app/home/index.js
+++ app/home/index.js
@@ -1,39 +1,40 @@
 import React from 'react';
-import Footer from '../footer'
+import Footer from '../footer';
 import Header from '../header';
 import Main from '../main';
 
-export default () => (
+export default () =>
   <div className="appContainer">
     <Header />
     <Main />
     <Footer />
-    <style jsx>{`
-      .appContainer {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-      }
-    `}
+    <style jsx>
+      {`
+        .appContainer {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+      `}
     </style>
-    <style jsx global>{`
-      .centerContainer {
-        width: 80%;
-        margin: 0px auto;
-      }
-      .brandingColor {
-        background-color: tomato;
-      }
-      body {
-        margin: 0px;
-      }
-      ul {
-        padding: 0;
-      }
-      li {
-        list-style-type: none;
-      }
-    `}
+    <style jsx global>
+      {`
+        .centerContainer {
+          width: 80%;
+          margin: 0px auto;
+        }
+        .brandingColor {
+          background-color: tomato;
+        }
+        body {
+          margin: 0px;
+        }
+        ul {
+          padding: 0;
+        }
+        li {
+          list-style-type: none;
+        }
+      `}
     </style>
-  </div>
-);
+  </div>;
