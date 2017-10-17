a6a441f572e319913b9c70d3268363d460b86a36
Yeiber <yeibercano@gmail.com>
2017-10-17 11:03:39 -0700

    create basic HOC for layouts

--- app/HoC/layouts/lobby.js
+++ app/HoC/layouts/lobby.js
@@ -1,1 +1,17 @@
 import React, { component } from 'react';
+import { Header } from './header';
+import { Home } from './header';
+import { Footer } from './header';
+
+export const withMain = lobby => Main => (
+  <Home>
+    <Header />
+    <Main />
+    <Footer />
+  </Home>
+);
+
+withMain.propTypes = {
+  lobby: PropTypes.string.isRequired
+  Main: PropTypes.func.isRequired
+};
