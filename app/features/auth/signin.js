fca4dd74a85874001683979b0a497d863154ee50
Yeiber <yeibercano@gmail.com>
2018-01-01 22:11:02 -0800

    fix styles signin

--- app/features/auth/signin.js
+++ app/features/auth/signin.js
@@ -1,20 +1,22 @@
+import React from 'react';
 import { authUser, auth, googleAuthProvider } from '../../storage/firebase';
 import { userLogginRequested } from '../../utilities/';
 
 const Signin = () => {
   return (
     <aside className="signinComponent">
-      <button className="signin" onClick={ () => userLogginRequested() }>
+      <button className="signin" onClick={() => userLogginRequested()}>
         Sign In
       </button>
-      <style jsx>{`
-        .signinComponent {
-          width: auto;
-        }
-      `}
+      <style jsx>
+        {`
+          .signinComponent {
+            width: auto;
+          }
+        `}
       </style>
     </aside>
   );
-}
+};
 
-export default Signin
+export default Signin;
